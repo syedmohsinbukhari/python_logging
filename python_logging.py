9724de02d77a483d11f3a0192ae9cf3c4e9ffb8d
syedmohsinbukhari <syedmohsinbukhari@googlemail.com>
2019-10-09 11:14:11 +0500

    Creates a file for storing logs if it does not exist

--- python_logging.py
+++ python_logging.py
@@ -1,18 +1,27 @@
+import os
 import logging
 from logging import info, debug
+
 
 def setup_logging(log_fname=''):
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
 
     if log_fname != '':
-        format_string = "%(asctime)s [%(filename)s:%(lineno)s - " + \
+        try:
+            if not os.path.exists(log_fname):
+                os.mknod(log_fname)
+
+            format_string = "%(asctime)s [%(filename)s:%(lineno)s - " + \
                         "%(funcName)s() ] [%(levelname)s] %(message)s"
-        file_handler = logging.FileHandler("{0}".format(log_fname))
-        file_formatter = logging.Formatter(format_string)
-        file_handler.setFormatter(file_formatter)
-        file_handler.setLevel(logging.DEBUG)
-        root_logger.addHandler(file_handler)
+            file_handler = logging.FileHandler(f"{log_fname}")
+            file_formatter = logging.Formatter(format_string)
+            file_handler.setFormatter(file_formatter)
+            file_handler.setLevel(logging.DEBUG)
+            root_logger.addHandler(file_handler)
+
+        except Exception as e:
+            info(e)
 
     console_handler = logging.StreamHandler()
     console_formatter = logging.Formatter(
@@ -21,6 +30,7 @@ def setup_logging(log_fname=''):
     console_handler.setFormatter(console_formatter)
     console_handler.setLevel(logging.INFO)
     root_logger.addHandler(console_handler)
+
 
 if __name__ == '__main__':
     setup_logging('./python_logging.log')
